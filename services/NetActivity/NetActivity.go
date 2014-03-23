291a2ecdb8a6f0a38bfe3ed30e7b3cc13b8e0429
SQP <meuarrr@gmail.com>
2014-03-23 17:54:24 +0100

    New applet: NetActivity - Monitor network activity. Rewrite of netspeed, with multi interfaces monitoring and more click actions. It's almost a clone of DiskSpeed

---
+++ services/NetActivity/NetActivity.go
@@ -0,0 +1,94 @@
+// Network activity monitoring applet for the Cairo-Dock project.
+package NetActivity
+
+import (
+	"github.com/sqp/godock/libs/cdtype"
+	"github.com/sqp/godock/libs/dock"     // Connection to cairo-dock.
+	"github.com/sqp/godock/libs/packages" // ByteSize.
+	"github.com/sqp/godock/libs/sysinfo"
+	"github.com/sqp/godock/libs/ternary" // Ternary operators.
+
+	"fmt"
+)
+
+//
+//------------------------------------------------------------------[ APPLET ]--
+
+// Applet data and controlers.
+//
+type Applet struct {
+	*dock.CDApplet
+	conf    *appletConf
+	service *sysinfo.IOActivity
+}
+
+// Create a new applet instance.
+//
+func NewApplet() *Applet {
+	app := &Applet{CDApplet: dock.NewCDApplet()} // Icon controler and interface to cairo-dock.
+
+	app.service = sysinfo.NewIOActivity(app)
+	app.service.FormatIcon = sysinfo.FormatIcon
+	app.service.FormatLabel = formatLabel
+	app.service.GetData = sysinfo.GetNetActivity
+
+	app.AddPoller(app.service.Check)
+
+	return app
+}
+
+// Load user configuration if needed and initialise applet.
+//
+func (app *Applet) Init(loadConf bool) {
+	app.LoadConfig(loadConf, &app.conf) // Load config will crash if fail. Expected.
+
+	// Settings for poller and diskSpeed (force renderer reset in case of reload).
+	app.conf.UpdateDelay = dock.PollerInterval(app.conf.UpdateDelay, defaultUpdateDelay)
+	app.service.Settings(uint64(app.conf.UpdateDelay), cdtype.InfoPosition(app.conf.DisplayText), app.conf.DisplayValues, app.conf.GraphType, app.conf.GaugeName, app.conf.Devices...)
+
+	// Set defaults to dock icon: display and controls.
+	app.SetDefaults(dock.Defaults{
+		Label:          ternary.String(app.conf.Name != "", app.conf.Name, app.AppletName),
+		PollerInterval: app.conf.UpdateDelay,
+		Commands: dock.Commands{
+			"left":   dock.NewCommandStd(app.conf.LeftAction, app.conf.LeftCommand, app.conf.LeftClass),
+			"middle": dock.NewCommandStd(app.conf.MiddleAction, app.conf.MiddleCommand)},
+		Debug: app.conf.Debug})
+}
+
+//
+//------------------------------------------------------------------[ EVENTS ]--
+
+// Define applet events callbacks.
+//
+func (app *Applet) DefineEvents() {
+
+	// Left and middle clicks: launch configured command.
+	app.Events.OnClick = app.LaunchFunc("left")
+	app.Events.OnMiddleClick = app.LaunchFunc("middle")
+
+	app.Events.OnBuildMenu = func() {
+		menu := []string{}
+		if app.conf.LeftAction > 0 {
+			menu = append(menu, "Action left click")
+		}
+		if app.conf.MiddleAction > 0 {
+			menu = append(menu, "Action middle click")
+		}
+		app.PopulateMenu(menu...)
+	}
+
+	app.Events.OnMenuSelect = func(i int32) {
+		list := []string{"left", "middle"}
+		app.LaunchCommand(list[i])
+	}
+}
+
+//
+//-----------------------------------------------------------------[ DISPLAY ]--
+
+// Label display callback. One line for each device. Format="eth0: ↓ 42 / ↑ 128".
+//
+func formatLabel(dev string, in, out uint64) string {
+	return fmt.Sprintf("%s: %s %s / %s %s", dev, "↓", packages.ByteSize(in), "↑", packages.ByteSize(out))
+}
---
+++ services/NetActivity/config.go
@@ -0,0 +1,61 @@
+package NetActivity
+
+const defaultUpdateDelay = 3
+
+//------------------------------------------------------------------[ CONFIG ]--
+
+type appletConf struct {
+	groupIcon          `group:"Icon"`
+	groupConfiguration `group:"Configuration"`
+	groupActions       `group:"Actions"`
+}
+
+type groupIcon struct {
+	Name string `conf:"name"`
+}
+
+type groupConfiguration struct {
+	DisplayText   int
+	DisplayValues int
+
+	GaugeName string
+	// GaugeRotate bool
+
+	GraphType int
+	// GraphColorHigh []float64
+	// GraphColorLow  []float64
+	// GraphColorBg   []float64
+	// GraphMix bool
+
+	UpdateDelay int
+	Devices     []string
+}
+
+type groupActions struct {
+	LeftAction    int
+	LeftCommand   string
+	LeftClass     string
+	MiddleAction  int
+	MiddleCommand string
+
+	// Still hidden.
+	Debug bool
+}
+
+/*
+Dropped from conf as there is no way to implement it from an external applet ATM.
+
+#l+[No;With dock orientation;Yes] Rotate applet theme :
+GaugeRotate=1
+
+#c+ High value's colour :
+#{It's the colour of the graphic for high rate values.}
+GraphColorHigh=1;0;0;
+
+#c+ Low value's colour :
+#{Graph colour for low rate vaues:}
+GraphColorLow=1;1;0;
+
+#C+ Background colour of the graphic :
+GraphColorBg=0.61438925764858476;0.61438925764858476;0.64814221408407724;0.7686274509803922;
+*/
